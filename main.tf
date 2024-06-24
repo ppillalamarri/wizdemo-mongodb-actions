b7b89738c45581fc2e08f5bf9b60829a1c875a6c
ppillalamarri <ppillalamarri@gmail.com>
2024-06-25 00:27:37 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_security_group" "web-sg" {
-  name = "launch-wizard-1"
+  name = "${random_pet.sg.id}-sg"
   ingress {
     from_port   = 8080
     to_port     = 8080
