e0d1b3a54e32bcbf7cb30f87a096922a800ba113
ppillalamarri <ppillalamarri@gmail.com>
2024-06-26 17:45:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+#Explanation:
+#VPC and Subnets: Creates a VPC with one public and one private subnet.
+#Security Groups: Defines security groups for MongoDB and MongoExpress.
+#EC2 Instance for MongoDB: Launches an EC2 instance in the public subnet with MongoDB running in a Docker container.
+#ECS Cluster for MongoExpress: Sets up an ECS Fargate cluster and task definition for the MongoExpress web application.
+#Load Balancer: Configures an ALB to route traffic to the MongoExpress service.
+#S3 Backup and IAM Role: Creates an S3 bucket for MongoDB backups and an IAM role/policy to allow the EC2 instance to perform backups to S3.
+
 terraform {
   required_providers {
     aws = {
@@ -15,6 +23,8 @@ terraform {
   }
   required_version = ">= 1.1.0"
 }
+
+
 
 provider "aws" {
   region = "eu-west-1"
