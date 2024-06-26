ba77f465c231789578418be7a39779bb90aaa80e
ppillalamarri <ppillalamarri@gmail.com>
2024-06-26 23:10:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,6 +28,8 @@ terraform {
 
 provider "aws" {
   region = "eu-west-1"
+  access_key = "AKIARATHADOVEYTEQYWI"
+  secret_key = "uuIl8NxNJAFVu7/VXLYKH0zmhrFXoRn9APXB8I6"
 }
 
 #This script accomplishes the following:
